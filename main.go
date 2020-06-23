9142cf05163b96dd3d2bf5bb879295482693306b
Alexander Profeit <alex@profeit.com>
2020-06-23 17:38:31 -0400

    Listen on port PORT

--- main.go
+++ main.go
@@ -2,14 +2,20 @@ package main
 
 import (
   "net/http"
+  "os"
   "github.com/rs/zerolog/log"
 )
 
 func main() {
+  port := os.Getenv("PORT")
+  if port == "" {
+    port = "8080"
+  }
+
   http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
     http.Redirect(w, r, "https://forms.gle/xHfspeCYAvgTCyPp7", 307)
   })
 
-  log.Info().Msg("Starting server")
-  log.Fatal().Err(http.ListenAndServe(":8080", nil))
+  log.Info().Msgf("Starting server on port %s", port)
+  log.Fatal().Err(http.ListenAndServe(":" + port, nil))
 }
